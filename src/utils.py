35f84a61fa3362775a6ad7cf63a4f4489f78e9de
Deivid <deivid.johan.botina.monsalve@gmail.com>
2023-07-12 16:11:18 +0200

    [UPD] PEP8 style added. Option to add the input file paths, and also avoiding overwriting the output file if it exists already

--- src/utils.py
+++ src/utils.py
@@ -4,29 +4,51 @@ import openai
 from dotenv import load_dotenv, find_dotenv
 import enum
 
-_ = load_dotenv(find_dotenv()) # read local .env file
+_ = load_dotenv(find_dotenv())  # read local .env file
 
 try:
-    openai.api_key  = os.environ['OPENAI_API_KEY']
+    openai.api_key = os.environ["OPENAI_API_KEY"]
 except Exception as err:
     print(f"Unexpected {err=}, {type(err)=}")
     print("Do you have the .env file with your OPENAI_API_KEY?")
     sys.exit()
 
-class languages(enum.Enum):
-    english = enum.auto()
-    spanish = enum.auto()
-    french = enum.auto()
-    german = enum.auto()
-    portuguese = enum.auto()
-    italian = enum.auto()
 
-def read_file(file_path:str):
-    with open(file_path, 'r') as file:
+class Languages(enum.Enum):
+    ENGLISH = enum.auto()
+    SPANISH = enum.auto()
+    FRENCH = enum.auto()
+    GERMAN = enum.auto()
+    PORTUGUESE = enum.auto()
+    ITALIAN = enum.auto()
+
+
+def read_file(file_path: str):
+    """
+    Read the content of a file.
+
+    Args:
+        file_path (str): Path to the file.
+
+    Returns:
+        str: Content of the file.
+    """
+    with open(file_path, "r") as file:
         content = file.read()
     return content
 
-def get_completion(prompt, model="gpt-3.5-turbo"): #"gpt-3.5-turbo"
+
+def get_completion(prompt, model="gpt-3.5-turbo"):  # "gpt-3.5-turbo"
+    """
+    Get completion from the language model.
+
+    Args:
+        prompt (str): The input prompt.
+        model (str): The language model to use.
+
+    Returns:
+        str: The generated completion.
+    """
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
@@ -35,18 +57,50 @@ def get_completion(prompt, model="gpt-3.5-turbo"): #"gpt-3.5-turbo"
     )
     return response.choices[0].message["content"]
 
+
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0, max_tokens=500):
+    """
+    Get completion from messages using the language model.
+
+    Args:
+        messages (list): List of messages.
+        model (str): The language model to use.
+        temperature (float): Temperature parameter for text generation.
+        max_tokens (int): Maximum number of tokens to generate.
+
+    Returns:
+        str: The generated completion.
+    """
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=temperature, 
-        max_tokens=max_tokens, 
+        temperature=temperature,
+        max_tokens=max_tokens,
     )
     return response.choices[0].message["content"]
 
-def save_text_file(text:str, name:str, format:str='txt'):
-    if format in ["txt"]:
-        with open(name, 'w') as f:
-            f.write(text)
-    else:
+def save_text_file(text: str, name: str, format: str = "txt"):
+    """
+    Save text to a file. If the file already exists, append a number to the filename.
+
+    Args:
+        text (str): The text to save.
+        name (str): The file name.
+        format (str): The file format.
+    """
+    if format != "txt":
         print("[save_text_file] ERROR: Format not valid")
+        return
+
+    base_name, ext = os.path.splitext(name)
+    counter = 1
+    new_name = name
+
+    while os.path.exists(new_name):
+        new_name = f"{base_name}({counter}){ext}"
+        counter += 1
+
+    with open(new_name, "w") as file:
+        file.write(text)
+
+    print(f"[SUCCESS!] File saved as {new_name}")
